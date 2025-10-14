7b55974ee770ec44d800d6f85ad529358eb0f3b4
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2025-10-14 12:32:03 +0600

    Create FCFS.py

---
+++ CPU Scheduling/FCFS.py
@@ -0,0 +1,42 @@
+n_p = int(input("Enter no of P : "))
+
+AT = []
+BT = []
+
+for i in range(n_p):
+    at = int(input(f"Enter AT for P{i} : "))
+    AT.append(at)
+    bt = int(input(f"Enter BT for P{i} : "))
+    BT.append(bt)
+
+process = list(range(n_p))
+gantt = []
+TAT = [0] * n_p
+WT = [0] * n_p
+current = 0
+process.sort(key=lambda x: AT[x])
+
+for p in process:
+    if AT[p] > current:
+        gantt.append("ideal")
+        current = AT[p]
+
+    current += BT[p]
+    TAT[p] = current - AT[p]
+    WT[p] = TAT[p] - BT[p]
+    gantt.append(f"P{p}")
+    
+
+print("Gantt chart ")
+print(" -> " .join(gantt))
+
+# Compute and print ATT and AWT
+AWT = sum(WT) / n_p
+ATT = sum(TAT) / n_p
+
+print("\nProcess\tArrival\t\t\tBurst\t\tWaiting\t\tTurnaround")
+for i in range(n_p):
+    print(f"P{i}\t\t\t{AT[i]}\t\t\t{BT[i]}\t\t\t{WT[i]}\t\t\t{TAT[i]}")
+
+print(f"\nAverage Waiting Time (AWT): {AWT:.2f}")
+print(f"Average Turnaround Time (ATT): {ATT:.2f}")
